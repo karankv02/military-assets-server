6f60030648ab396fe00fddcc94d2de8d5576cc53
karankv02 <151569261+karankv02@users.noreply.github.com>
2025-06-30 23:45:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const prisma = new PrismaClient();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // React frontend
+  origin: procees.env.FRONT_END, // React frontend
   credentials: true
 }));
 
